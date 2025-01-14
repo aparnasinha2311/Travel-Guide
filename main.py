ff39ae08e36e1cd262b1728c982278b86f6ffb8e
aparnasinha2311 <aparnasinha2311@gmail.com>
2025-01-14 15:12:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,12 +24,12 @@ st.header("Travel Guide")
 
 st.subheader("Generate travel ideas using Gen AI")
 
-location= st.text("India/ International/ India & International")
+location= st.text_input("India/ International/ India & International")
 person= st.number_input("Number of people going on the trip", min_value=1, max_value= 50, value=1, step=1)
-tourtype= st.text("Type of tour- Family/ Romantic/ Adventure/ Relaxing/ Religious/ Wildlife/ Water activities/ Beach/ Mountain")
-month= st.text("Which month do you plan to go")
+tourtype= st.text_input("Type of tour- Family/ Romantic/ Adventure/ Relaxing/ Religious/ Wildlife/ Water activities/ Beach/ Mountain")
+month= st.text_input("Which month do you plan to go")
 days= st.number_input("Number of days of trip", min_value=1, max_value= 365, value=1, step=1)
-budget= st.text("Budget Amount= ")
+budget= st.text_input("Budget Amount= ")
 
 if st.button("Generate"):
     tours = tour_chain.invoke({"location": location,"person": person,"tourtpe": tourtype,"month": month,"days": days, "budget": budget})
